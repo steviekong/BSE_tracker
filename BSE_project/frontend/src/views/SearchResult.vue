ef58ae02fbb0880ba6fe23e7c69f38a324e0b96d
steviekong <sidharth.reji@gmail.com>
2021-04-17 03:23:06 +0530

    fixed cors issue

--- BSE_project/frontend/src/views/SearchResult.vue
+++ BSE_project/frontend/src/views/SearchResult.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- eslint-disable max-len -->
-  <div class="w-full h-full justify-center items-center flex">
+  <div class="w-full h-full justify-center text-center mt-8 flex">
     <div class="w-3/5 h-3/5">
       <div>
         <p class="font-serif text-5xl text-pink-500">
@@ -8,28 +8,32 @@
         </p>
         <p class="text-2xl">We actually pay you to invest</p>
       </div>
-      <div class="w-full h-12 px-4 pt-3 rounded-2xl border-blue-500 border mt-8">
-        <input class="outline-none w-full" type="text" for="search" id="search" v-model="search" placeholder="Company Name">
+      <div>
+      <p> {{ info }} </p>
       </div>
-      <button @click="onSearchClick" class="w-52 h-12 border bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 rounded-lg text-white mt-10 text-xl font-sans hover:opacity-50">
-        Search
-      </button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import axios from 'axios';
 
 export default Vue.extend({
   name: 'Result',
   data() {
-    return { search: '' };
+    return {
+      info: null,
+    };
   },
-  methods: {
-    onSearchClick() {
-      this.$router.push(`/search/${this.search}`);
-    },
+  mounted() {
+    axios
+      .get('http://localhost:8000/search', {
+        params: {
+          search_string: 'S',
+        },
+      })
+      .then((response) => { (this.info = response); });
   },
 });
 </script>

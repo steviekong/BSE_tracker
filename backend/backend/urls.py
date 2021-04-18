afb7d51a0ec5470b961e76c9fc23350bc8ee647f
steviekong <sidharth.reji@gmail.com>
2021-04-18 14:36:15 +0530

    fix routing issues

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 import requests
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('search', views.search_by_name, name='search'),
-    path('download', views.download_results_csv, name='download_results_csv'),
+    path('api/search', views.search_by_name, name='search'),
+    path('api/download', views.download_results_csv,
+         name='download_results_csv'),
+    re_path(r'^(?:.*)/?$', views.index),
 ]

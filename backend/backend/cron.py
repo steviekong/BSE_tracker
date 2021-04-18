0364d183d0fb87736e9d0fcc0ce370959e3c3535
steviekong <sidharth.reji@gmail.com>
2021-04-18 14:01:02 +0530

    change file store to /tmp

--- backend/backend/cron.py
+++ backend/backend/cron.py
@@ -25,21 +25,21 @@ def get_bahv_add_to_redis() -> None:
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
         BSE_zip_file = requests.get(
             "https://www.bseindia.com/download/BhavCopy/Equity/" + file_name, headers=headers)
-        open(file_name, "wb").write(BSE_zip_file.content)
+        open("/tmp/" + file_name, "wb").write(BSE_zip_file.content)
 
-        with zipfile.ZipFile(file_name, "r") as zip_ref:
-            zip_ref.extractall(".")
+        with zipfile.ZipFile("/tmp/" + file_name, "r") as zip_ref:
+            zip_ref.extractall("/tmp")
 
         # Convert csv rows into json string and insert into redis with the name as the key
-        with open("EQ" + current_date+".CSV", encoding='utf-8') as csv_file:
+        with open("/tmp/EQ" + current_date+".CSV", encoding='utf-8') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             for row in csv_reader:
                 data = json.dumps({"Code": row["SC_CODE"].rstrip(), "Name": row["SC_NAME"].rstrip(), "Open": row["OPEN"].rstrip(),
                                    "High": row["HIGH"].rstrip(), "Low": row["LOW"].rstrip(), "Close": row["CLOSE"].rstrip()})
                 key = row["SC_NAME"].rstrip()
                 redis_connection.set(key, data)
-        os.remove(file_name)
-        os.remove("EQ" + current_date+".CSV")
+        os.remove("/tmp/" + file_name)
+        os.remove("/tmp/EQ" + current_date+".CSV")
 
         print("Successfully parse and inserted BSE data for " + current_date)
 
